dd11bad9212ff6f4d73b186c8c0b66bab1962d31
micnil <michaelnilsson90@hotmail.com>
2014-09-20 13:17:14 +0800

    added sprite for character and created controller for rigidbody and without

---
+++ Freakolution/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	private float speed = 20f;
+	
+	
+	void FixedUpdate()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		
+		Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+		
+		rigidbody.velocity = moveDirection * speed;
+		//rigidbody.AddForce(moveDirection*speed);
+	}
+}
