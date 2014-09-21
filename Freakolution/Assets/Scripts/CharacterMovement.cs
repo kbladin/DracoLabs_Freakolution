aacebebc4f2262ee925a46676ad1df5a149e67d5
kbladin <kallebladin@gmail.com>
2014-09-21 16:52:21 +0800

    Animation in different directions. Problem: The speed of the character is 0.2 in the beginning. Dont know why.

--- Freakolution/Assets/Scripts/CharacterMovement.cs
+++ Freakolution/Assets/Scripts/CharacterMovement.cs
@@ -4,12 +4,9 @@ using System.Collections;
 public class CharacterMovement : MonoBehaviour {
 
 	private float speed = 20f;
-
-	public Animator[] animators;
-
+	
 	void Start()
 	{
-		animators = gameObject.GetComponentsInChildren<Animator>();
 	}
 	
 	void FixedUpdate()
@@ -18,12 +15,9 @@ public class CharacterMovement : MonoBehaviour {
 		float verticalInput = Input.GetAxis("Vertical");
 		
 		Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+		float horizontalAngle = Mathf.Atan2(verticalInput, horizontalInput);
 		
 		rigidbody.velocity = moveDirection * speed;
 		//rigidbody.AddForce(moveDirection*speed);
-
-		// Animation data
-		//Mathf.Abs (rigidbody.velocity);
-		animators [0].SetFloat ("speed", rigidbody.velocity.magnitude);
 	}
 }
---
+++ Freakolution/Assets/Scripts/CharacterSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSprite : MonoBehaviour {
+	
+	private Rigidbody parent_rigid_body;
+	
+	public Animator anim;
+	
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		parent_rigid_body = GetComponentInParent<Rigidbody>();
+	}
+	
+	void FixedUpdate()
+	{
+		float horizontalAngle = Mathf.Atan2(parent_rigid_body.velocity.z, parent_rigid_body.velocity.x);
+
+		anim.SetFloat ("speed", parent_rigid_body.velocity.magnitude);
+		anim.speed = parent_rigid_body.velocity.magnitude / 15;
+		anim.SetFloat ("horizontalAngle", horizontalAngle);
+	}
+}
