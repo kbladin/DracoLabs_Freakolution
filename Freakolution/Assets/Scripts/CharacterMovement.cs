6ec09d4a03aa0e0fcfa7488cdb71b0c25ee120a9
micnil <michaelnilsson90@hotmail.com>
2014-09-23 16:13:00 +0800

    changed character controller

--- Freakolution/Assets/Scripts/CharacterMovement.cs
+++ Freakolution/Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,23 @@ using System.Collections;
 public class CharacterMovement : MonoBehaviour {
 
 	private float speed = 20f;
+	public Vector3 moveDirection;
 	
+	void Start()
+	{
+		moveDirection = Vector3.forward;
+	}
 	
 	void FixedUpdate()
 	{
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
 		
-		Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
-		
+		if(horizontalInput != 0 && verticalInput != 0)
+		{
+
+		}
+		moveDirection = new Vector3(horizontalInput, 0, verticalInput);
 		rigidbody.velocity = moveDirection * speed;
 		//rigidbody.AddForce(moveDirection*speed);
 	}
---
+++ Freakolution/Assets/Scripts/DistanceDetection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceDetection : MonoBehaviour {
+
+	public float distance;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+			Ray rayCast = new Ray(transform.position, Vector3.forward);
+			
+			
+			if(Physics.Raycast(rayCast, out hit))
+			{
+				Debug.Log(hit.distance);
+				
+				//hit.collider.GetComponent<PlayerScripts
+			}
+		}
+		
+		
+		
+	}
+}
