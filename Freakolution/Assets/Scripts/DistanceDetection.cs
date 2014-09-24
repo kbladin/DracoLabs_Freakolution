6ef19adfb798dc1c36528d64bd6b2ef34874168f
micnil <michaelnilsson90@hotmail.com>
2014-09-24 16:46:56 +0800

    working distance detection

--- Freakolution/Assets/Scripts/DistanceDetection.cs
+++ Freakolution/Assets/Scripts/DistanceDetection.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class DistanceDetection : MonoBehaviour {
 
 	public float distance;
-	
+	ThirdPersonController controller;
 	// Update is called once per frame
+	void Awake(){
+		controller = GetComponent<ThirdPersonController>();
+	}
+	
 	void Update () {
 	
 		if(Input.GetButtonDown("Fire1"))
 		{
 			RaycastHit hit;
-			Ray rayCast = new Ray(transform.position, Vector3.forward);
+			Vector3 rayDirection = controller.GetDirection();
+			Ray rayCast = new Ray(transform.position, rayDirection);
 			
 			
 			if(Physics.Raycast(rayCast, out hit))
