60cc9e540976358dfc596777cd127018b6b49c6a
micnil <michaelnilsson90@hotmail.com>
2014-10-12 12:25:14 +0800

    moved distance detection into enemy and player script

---
+++ Freakolution/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float health;
+	private float attackRange;
+	private float damage;
+	//needs this to get movement direction
+	ThirdPersonController controller;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		attackRange = 3f;
+		damage = 30f;
+		health = 200f;
+		controller = GetComponent<ThirdPersonController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Attack();
+		}
+		if(health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	public void LoseHealth(float damage) 
+	{
+		//here needs to be damage formula
+		this.health -= damage;
+	}
+	
+	private void Attack() 
+	{
+		RaycastHit hit;
+		Vector3 rayDirection = controller.GetDirection();
+		Ray rayCast = new Ray(transform.position, rayDirection);
+		
+		if(Physics.Raycast(rayCast, out hit))
+		{	
+			if(hit.distance < attackRange && hit.transform.tag=="Enemy"){
+				
+				Enemy enemyComponent = hit.transform.GetComponent<Enemy>();
+				enemyComponent.LoseHealth(damage);
+			}
+		}
+	}
+	
+	
+}
