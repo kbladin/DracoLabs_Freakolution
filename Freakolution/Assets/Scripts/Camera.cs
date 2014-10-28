50db5d0f6bfef1d3ad74f98294eab979901ddf8b
kbladin <kallebladin@gmail.com>
2014-10-28 21:45:00 +0800

    Particle system when enemies attack.

--- Freakolution/Assets/Scripts/Camera.cs
+++ Freakolution/Assets/Scripts/Camera.cs
@@ -24,7 +24,7 @@ public class Camera : MonoBehaviour {
 		Vector3 meanPos = new Vector3(0,0,0);
 		foreach (GameObject player in players) {
 			if (player.GetComponent<Player>().Alive)
-			meanPos += player.transform.position;
+				meanPos += player.transform.position;
 		}
 		meanPos /= players.Length;
 
