eba7dd691b358e50f03fc33feebfab469d793962
kbladin <kallebladin@gmail.com>
2014-10-31 10:38:26 +0800

    Changed a bit in the menu scene and fixed puke bug.

---
+++ Freakolution/Assets/Scripts/Barrel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.RotateAround (transform.position, new Vector3(0,1,0), Random.Range(0f,360f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Freakolution/Assets/Scripts/Puke.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puke : MonoBehaviour {
+
+	public Chemicals pukeChemicals;
+	public float damage;
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetPlayer( Player p) {
+		player = p;
+	}
+	public void SetPukeChemicals(Chemicals c) {
+		pukeChemicals = c;
+	}
+	public void SetDamage(float d) {
+		damage = d;
+	}
+
+	void OnParticleCollision(GameObject other) {
+		if (other.tag == "Enemy") {
+			Enemy enemy = other.GetComponent<Enemy>();
+			enemy.LoseHealth(damage, pukeChemicals, player);
+			//Destroy(other);// body.AddForce(direction * 5);
+		}
+	}
+}
