a0b142fa56b2d3a4bef825e5d800b4f6ab7b689a
Sacha Béraud <sacha.beraud@gmail.com>
2014-11-01 21:05:56 +0800

    main menu improvement with ready states

---
+++ Freakolution/Assets/Scripts/GameVariables.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameVariables{
+
+	public static int nPlayers = 0;
+	public static string[] playerClasses;
+	public static int[] playerChemicals;
+	public static bool[] playersPlaying;
+	public static bool isMusicOn = true;
+	public static bool isSFXOn = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
