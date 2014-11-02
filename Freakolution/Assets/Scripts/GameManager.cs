344a2cc617e6457ea76bb27e5713d6d539d0297d
kbladin <kallebladin@gmail.com>
2014-11-02 11:51:18 +0800

    Fixed the problem with scoreboard.

--- Freakolution/Assets/Scripts/GameManager.cs
+++ Freakolution/Assets/Scripts/GameManager.cs
@@ -22,8 +22,6 @@ public class GameManager : MonoBehaviour {
 	private int numOfPlayers = 4;
 	
 	void Awake () {
-	
-		
 		players = new List<GameObject>();
 		numOfPlayers = GameVariables.nPlayers;
 //		print("n players = " + );
@@ -40,13 +38,11 @@ public class GameManager : MonoBehaviour {
 				GameObject player = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation) as GameObject;
 				player.GetComponent<ThirdPersonController>().SetPlayerNumber(i);
 				player.GetComponent<Player>().SetChemicals(new Chemicals(chemical));
+				player.GetComponent<Player>().stats.playerNumber = i;
 				player.transform.Find("CharacterSprite").transform.Find("PlayerNumberText").GetComponent<TextMesh>().text = (i+1).ToString();
 				players.Add (player);
-
 			}	
-
 		}
-
 
 
 //		for(int i = 0; i<numOfPlayers;i++)
