26de8cad7a790dd88185437f4a4ff0846b0ea95d
micnil <michaelnilsson90@hotmail.com>
2014-11-03 11:09:49 +0800

    luddes randomspawn

--- Freakolution/Assets/Scripts/EnemyCounter.cs
+++ Freakolution/Assets/Scripts/EnemyCounter.cs
@@ -4,19 +4,18 @@ using System.Collections;
 public class EnemyCounter : MonoBehaviour {
 	
 	private int numOfEnemies;
-	// Use this for initialization
+
 	void Start () {
 		numOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
-		GetComponent<GUIText>().text ="Enemies: " + numOfEnemies.ToString();
+		GetComponent<GUIText>().text ="Scientists: " + numOfEnemies.ToString();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		int newNumOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
 		if(newNumOfEnemies != numOfEnemies)
 		{
 			numOfEnemies = newNumOfEnemies;
-			GetComponent<GUIText>().text ="Enemies: " + numOfEnemies.ToString();
+			GetComponent<GUIText>().text ="Scientists: " + numOfEnemies.ToString();
 		}
 			
 	}
