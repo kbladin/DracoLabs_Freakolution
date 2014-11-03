2296a854801fc256a230eccda3617bc306d61019
kbladin <kallebladin@gmail.com>
2014-11-03 11:24:50 +0800

    Planes changing color when selecting chemicals.

---
+++ Freakolution/Assets/Scripts/PlayerFramePlane.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFramePlane : MonoBehaviour {
+
+	public GameObject referenceChemicalName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (referenceChemicalName.GetComponent<TextMesh> ().text == "Redium") {
+			GetComponent<MeshRenderer>().material.color = new Color(1f,0,0) * 0.5f;
+		}
+		else if (referenceChemicalName.GetComponent<TextMesh> ().text == "Greenogen") {
+			GetComponent<MeshRenderer>().material.color = new Color(0,1f,0) * 0.5f;
+		}
+		else if (referenceChemicalName.GetComponent<TextMesh> ().text == "Bluorine") {
+			GetComponent<MeshRenderer>().material.color = new Color(0,0,1f) * 0.5f;
+		}
+		else {
+			GetComponent<MeshRenderer>().material.color = new Color(0.4f,0.4f,0.4f);
+		}
+	}
+}
